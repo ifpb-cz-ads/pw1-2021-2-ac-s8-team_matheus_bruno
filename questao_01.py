d21f19894c256987100b16a01af449a547a6d610
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-02-02 10:06:48 -0300

    Resolvendo questão 01

---
+++ questao_01.py
@@ -0,0 +1,10 @@
+#1) Escreva uma função que retorne o maior de dois números. Valores esperados:
+
+#máximo(5,6) == 6
+#máximo(2,1) == 2
+#máximo(7,7) == 7
+
+def teste(numero1, numero2):
+    print(max(numero1,numero2))
+
+teste(7,7)
